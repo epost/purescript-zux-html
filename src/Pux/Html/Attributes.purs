9a952568fb935f9ce780d1cdcfceca2628f4a20e
Erik Post <erik@shinsetsu.nl>
2016-07-05 23:00:26 +0200

    Introduce 'href' and 'attr'.

--- src/Pux/Html/Attributes.purs
+++ src/Pux/Html/Attributes.purs
@@ -6,10 +6,17 @@ import Data.Foldable (foldMap)
 import Pux.Html (Attribute)
 
 className :: forall a. String -> Attribute a
-className c = "class='" <> c <> "'"
+className = attr "class"
+
+href :: forall a. String -> Attribute a
+href = attr "href"
 
 style :: forall a. Array (Tuple String String) -> Attribute a
 style pairs = "style='" <> foldMap f pairs <> "'"
   where
     f :: Tuple String String -> String
     f (Tuple k v) =  k <> ":" <> v <> ";"
+
+-- TODO Pux signature is: attr :: forall a v. String -> v -> Attribute a
+attr :: forall a. String -> String -> Attribute a
+attr attrKey v = attrKey <> "='" <> v <> "'"
